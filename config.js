ca0968a578f719734c1baa8a88ed251b08521b28
Jeff Vega <jeff@intellibliss.com>
2018-03-20 10:16:49 -0700

    updated database_url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/restaurants-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://dev:dev@ds053176.mlab.com:53176/jeff-restaurants-app';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-restaurants-app';
 exports.PORT = process.env.PORT || 8080;
